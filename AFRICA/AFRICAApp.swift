aa60cd4af53aa0c3fc988ad4d00df728928f8768
Batraju Nagaraju <batraju.nagaraju@alticeusa.com>
2022-12-07 09:50:39 +0530

    Initial Commit

---
+++ AFRICA/AFRICAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFRICAApp.swift
+//  AFRICA
+//
+//  Created by batraju.nagaraju on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AFRICAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
